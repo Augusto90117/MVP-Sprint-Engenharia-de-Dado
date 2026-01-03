77f41bf4ca523df32cb20667b0a994e5656b6e55
Augusto90117 <augustotafe@gmail.com>
2026-01-03 01:40:07 -0300

    Add extract function to read CSV data

--- etl/extract.py
+++ etl/extract.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from pathlib import Path
+
+def extract():
+    source = "data/raw/ibm_hr_raw.csv"
+    df = pd.read_csv(source)
+    print("Extração concluída")
+
+if __name__ == "__main__":
+    extract()
 
